94978b9a234afbbabc4da64d1433680ce7fc4d2f
AshrafJaman <85231517+AshrafJaman@users.noreply.github.com>
2022-05-13 23:16:27 +0600

    error fix

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,6 +1,13 @@
+import Topbar from "../../components/topbar/Topbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+
+
 
 export default function Home(){
     return (
-        <div> This is homepage</div>
-    )
+    <>
+        <Topbar/>
+        <Sidebar/>
+    </>
+    );
 }
